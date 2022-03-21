e8a4341a2ce28c3e85a29baa629a57f1e842d1be
Egor Zhukov <bit.zhukov@gmail.com>
2022-03-21 16:29:25 +0300

    add FeaturePreprocessing

--- data_prep.py
+++ data_prep.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 import math as m
 import random as rd
 import torch
+import torch.nn as nn
+from typing import Dict, Union, Optional, Any, Iterable
 
 
 class NpEncoder(json.JSONEncoder):
@@ -34,7 +36,7 @@ class TabularSequentialDataset(Dataset):
         num_cols = len(df.columns)
         for rid, rdata in tqdm(df.iterrows(), total=len(df)):
             rdata = np.stack(rdata.values, axis=-1)
-            rdata_padded = np.zeroes((max_seq_len, num_cols))
+            rdata_padded = np.zeros((max_seq_len, num_cols))
             rdata_padded[-len(rdata):, :] = rdata
             df.loc[rid] = rdata_padded.T.tolist()
         return df
@@ -56,3 +58,42 @@ class TabularSequentialDataset(Dataset):
                                         device=self.device)
         return tensors
 
+
+class FeaturePreprocessing(nn.Module):
+    def __init__(self, schema: Dict[str, str], hidden_dim: int=64, training: bool=True):
+        super(FeaturePreprocessing, self).__init__()
+        self.training = training
+        self.embedding = dict()
+        self.hidden_dim = hidden_dim
+        self.features_dim = 0
+        self.features_order = list()
+        for feat, stats in schema.items():
+            if stats['type'] == 'categorical':
+                self.embedding[feat] == nn.Embedding(num_embeddings=stats['max_val']+1,
+                                                     embedding_dim=stats['embedding_dim'])
+                self.features_dim += stats['embedding_dim']
+            else:
+                self.features_dim += 1
+            self.features_order.append(feat)
+        self.normalize = nn.BatchNorm1d(num_features=self.features_dim)
+        self.regularize = nn.Dropout(p=0.1369)
+        self.full_connect = nn.Linear(in_features=self.features_dim,
+                                      out_features=self.hidden_dim, bias=True)
+        self.activation = nn.Mish()
+
+    def forward(self, tensor: Dict[str, torch.Tensor]):
+        features = []
+        for feat in self.features_order:
+            if feat in self.embedding.keys():
+                feat_tensor = self.embedding[feat](tensor[feat])
+                feat_tensor = torch.swapaxes(feat_tensor, axis0=1, axis1=2)
+            else:
+                feat_tensor = torch.unsqueeze(tensor[feat], dim=1)
+            features.append(feat_tensor)
+        features = torch.cat(features, dim=1)
+        features = self.normalize(features)
+        features = torch.swapaxes(features, axis0=1, axis1=2)
+        features = self.regularize(features)
+        features = self.full_connect(features)
+        features = self.activation(features)
+        return features
